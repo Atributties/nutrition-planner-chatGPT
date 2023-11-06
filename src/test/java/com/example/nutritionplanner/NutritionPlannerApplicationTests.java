f3236bd70d30bea43a4b18fa4c6b064dff9acbee
MT <113047767+Mathias-Thomsen@users.noreply.github.com>
2023-11-06 12:29:09 +0100

    First Commit.

---
+++ src/test/java/com/example/nutritionplanner/NutritionPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nutritionplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritionPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
